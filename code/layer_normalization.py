8947f5485d552e5cd2518b9e1c41d831c26b58d1
shaohua-li <shaohuali.hkust@gmail.com>
2015-05-13 00:36:21 +0800

    modified normalization layer using local response normalization as proposed in AlexNet

--- code/layer_normalization.py
+++ code/layer_normalization.py
@@ -2,14 +2,14 @@ import theano
 import theano.tensor as T
 import numpy
 
-from utils import local_responce_normalization
+from utils import local_responce_normalization_
 
 class NormalizationLayer(object):
     """ Class for normalization, use local responce normalization.
     """
-    def __init__(self, data, eps=0.001):
+    def __init__(self, data, k=2, n=5, alpha=0.0001, beta=0.75):
         """ Constructor.
             data: 4D tensor as input from previous layer.
             eps:  small constant in case denominator is 0.
         """
-        self.output = local_responce_normalization(data, eps) 
+        self.output = local_responce_normalization_(data, k, n, alpha, beta) 
