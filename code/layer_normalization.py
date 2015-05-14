b1b68b63f512140ad7240f4b5cfc6b8ff0df9193
shaohua-li <shaohuali.hkust@gmail.com>
2015-05-14 17:42:00 +0800

    added normalization mode term in normalization layer

--- code/layer_normalization.py
+++ code/layer_normalization.py
@@ -2,14 +2,20 @@ import theano
 import theano.tensor as T
 import numpy
 
-from utils import local_responce_normalization
+from utils import local_responce_normalization_across
 
 class NormalizationLayer(object):
     """ Class for normalization, use local responce normalization.
     """
-    def __init__(self, data, k=2, n=5, alpha=0.0001, beta=0.75):
+    def __init__(self, data, n=5, alpha=0.0001, beta=0.75, mode=0):
         """ Constructor.
-            data: 4D tensor as input from previous layer.
-            eps:  small constant in case denominator is 0.
+            data  : 4D tensor as input from previous layer.
+            n     : size of receptive field
+            alpha : addition coefficient
+            beta  : exponential term
+            mode  : 0 for across channel, non-zero for within channel
         """
-        self.output = local_responce_normalization(data, k, n, alpha, beta) 
+        if (mode == 0):
+            self.output = local_responce_normalization_across(data, n, alpha, beta) 
+        else:
+            self.output = local_responce_normalization_within(data, n, alpha, beta)
