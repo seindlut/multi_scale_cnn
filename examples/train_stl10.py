e022c6617507cb087d2f17c59c4f5b8b6e9e81cf
shaohua-li <shaohuali.hkust@gmail.com>
2015-05-25 23:31:19 +0800

    added training function for stl10

--- examples/train_stl10.py
+++ examples/train_stl10.py
@@ -1,1 +1,15 @@
+import setup
+import pdb
 
+from src.IO import load_stl10_train
+
+
+def train_stl10(file_name):
+    """ Function for training stl10 dataset """
+    train_x, train_y = load_stl10_train(file_name)
+    pdb.set_trace()
+
+if __name__ == '__main__':
+    path = '../data/stl10/'
+    fname = 'train'
+    train_stl10(path+fname)
